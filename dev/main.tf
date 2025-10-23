e20045075210e0f1000d623f37b3648ad5f97190
Leandro López <inkel.ar@gmail.com>
2025-10-23 15:34:41 -0300

    Add null_resource for drift detection

--- dev/main.tf
+++ dev/main.tf
@@ -4,3 +4,11 @@ module "dev" {
   # Change 0 to 1 and open a pull request to trigger Terrateam
   null_resource_count = 1
 }
+
+resource "null_resource" "drift" {
+  count = 2
+
+  provisioner "local-exec" {
+    command = "echo 'DRIFT DRIFT DRIIIIIIIFT!!!'"
+  }
+}
